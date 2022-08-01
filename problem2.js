d2b448aefa77eef6075fac3837a0eea56171533d
nomayen <nomayen.ohin@gmail.com>
2022-08-02 00:25:10 +0600

    push comitted

--- problem2.js
+++ problem2.js
@@ -1,23 +1,80 @@
-var number = 100;
-var ohin = 85 ;
-var tom = 66 ;
-var janes = 95 ;
-var peters = 56 ;
-var johns = 40 ;
-
-
-if (ohin > 80) {
-    console.log('you got A+');
+var myScore = 85;
+var tom = 66;
+var jane = 95;
+var peter = 56;
+var john = 40;
+if (myScore >= 80){
+console.log('A+ Grade')
 }
-else if (tom > 60 ) {
-    console.log('tom got A');  
+else if (myScore >= 60){
+console.log('A Grade')
 }
-else if (janes > 95 ) {
-    console.log(' janes got A');  
+else if (myScore >= 50){
+console.log('B Grade')
 }
-else if (janes > 60 ) {
-    console.log(' janes got A');  
+else if (myScore >= 40){
+console.log('C Grade')
 }
-else if (tom > 60 ) {
-    console.log('tom got A');  
+else {
+console.log('F Grade')
+}
+if (tom >= 80){
+console.log('A+ Grade')
+}
+else if (tom >= 60){
+console.log('A Grade')
+}
+else if (tom >= 50){
+console.log('B Grade')
+}
+else if (tom >= 40){
+console.log('C Grade')
+}
+else {
+console.log('F Grade')
+}
+if (jane >= 80){
+console.log('A+ Grade')
+}
+else if (jane >= 60){
+console.log('A Grade')
+}
+else if (jane >= 50){
+console.log('C Grade')
+}
+else if (jane >= 40){
+console.log('C Grade')
+}
+else {
+console.log('F Grade')
+}
+if (peter >= 80){
+console.log('A+ Grade')
+}
+else if (peter >= 60){
+console.log(' A Grade')
+}
+else if (peter >= 50){
+console.log('C Grade')
+}
+else if (peter >= 40){
+console.log('C Grade')
+}
+else {
+console.log('F Grade')
+}
+if (john >= 80){
+console.log('A+ Grade')
+}
+else if (john >= 60){
+console.log('B Grade')
+}
+else if (john >= 50){
+console.log('C Grade')
+}
+else if (john >= 40){
+console.log('C Grade')
+}
+else {
+console.log('F Grade')
 }
