41253fab0ff97cb13af336da764bb93e97483537
nomayen <nomayen.ohin@gmail.com>
2022-08-02 16:32:49 +0600

    problem 3

---
+++ problem3.js
@@ -0,0 +1,24 @@
+
+var num1 = 79;
+var num2 = 13;
+var num3 = 45;
+if (num1 > num2 && num1 > num3) {
+console.log('num1 is the largest')
+}
+else if (num2 > num1 && num2 > num3) {
+console.log('num2 is the largest')
+}
+else if (num3 > num1 && num3 > num2) {
+console.log('num3 is the largest')
+}
+
+
+// problem-3.2
+var angel1 = 9;
+var angel2 = 8;
+var angel3 = 9;
+if (angel1 == angel2 || angel1 == angel3 || angel2 == angel3) {
+console.log("this triangle is isosceles");
+} else {
+console.log("this triangle is not isosceles");
+}
